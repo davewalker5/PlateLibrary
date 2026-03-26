e97871c1a82658e4d15175ea632b69bb04178af6
Dave Walker <david.walker5@icloud.com>
2026-03-26 13:34:56 +0000

    Surface the database path in the Datasette UI

--- scripts/run-datasette.sh
+++ scripts/run-datasette.sh
@@ -3,5 +3,22 @@
 export PROJECT_ROOT=$( cd "$( dirname "$0" )/.." && pwd )
 . $PROJECT_ROOT/venv/bin/activate
 
+# If the MICROSCOPY_PLATE_LIBRARY environment variable is set, the database is in the
+# folder specified in that variable. Otherwise, the database in the data folder of the
+# repository is used
 DB_PATH="${MICROSCOPY_PLATE_LIBRARY:-$PROJECT_ROOT/data}/plate_library.db"
-datasette "$DB_PATH" --open
+
+# The metadata is visible in the Datasette UI, so this has the effect of showing the
+# path to the current database (useful if switching between development and live databases)
+datasette "$DB_PATH" \
+  --metadata <(cat <<EOF
+{
+  "databases": {
+    "plate_library": {
+      "description": "Database Path: $DB_PATH"
+    }
+  }
+}
+EOF
+) \
+  --open
