d7660039edab6088a7f74af79efa73adf7640492
Dave Walker <david.walker5@icloud.com>
2026-03-31 11:29:43 +0100

    Make sure PROJ_DIR is set

--- scripts/run-dev-editor.sh
+++ scripts/run-dev-editor.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# For older versions of MacOS, pip may not find the proj wheel and will build it from source. The
+# environment then won't be set correctly to run proj, resulting in errors in the application. Set
+# the PROJ_DATA manually if it's present on the machine. The following assumes a MacPorts install
+# of proj9
+if [ -d "/opt/local/lib/proj9/share/proj" ]; then
+    export PROJ_DATA=/opt/local/lib/proj9/share/proj
+fi
+
 export PROJECT_ROOT=$( cd "$( dirname "$0" )/.." && pwd )
 . $PROJECT_ROOT/venv/bin/activate
 
